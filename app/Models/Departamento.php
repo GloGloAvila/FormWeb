cf577ea3ba3f58b45b44c1c5fe109a49d6e17a7b
Jhony Rocha <jhony.alexander.rocha@gmail.com>
2020-07-13 09:29:48 -0500

    Generando modelo y migración de departamentos

---
+++ app/Models/Departamento.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Log;
+
+class Departamento extends Model
+{
+
+  use SoftDeletes;
+
+  // Constantes para saber si el registro está activo
+  const REGISTRO_ACTIVO = '1';
+  const REGISTRO_INACTIVO = '0';
+
+  protected $table = 'departamentos';
+  protected $dates = ['deleted_at'];
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'nombre',
+    'codigo',
+    'divipola',
+    'activo'
+  ];
+
+  /**
+   * The attributes that should be hidden for arrays.
+   *
+   * @var array
+   */
+  protected $hidden = [
+    'created_at',
+    'updated_at',
+    'deleted_at'
+  ];
+
+  
+  // Función para saber si un registro está activo
+  public function estaActivo()
+  {
+    return $this->activo == Departamento::REGISTRO_ACTIVO;
+  }
+
+}
---
+++ database/migrations/2020_06_21_120004_create_departamentos_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+use App\Models\Departamento;
+
+class CreateDepartamentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departamentos', function (Blueprint $table) {
+          $table->increments('id');
+          $table->string('nombre');
+          $table->string('codigo');
+          $table->string('divipola');
+          $table->boolean('activo')->default(Departamento::REGISTRO_ACTIVO);
+
+          $table->timestamps();
+          $table->softDeletes()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      DB::statement('SET FOREIGN_KEY_CHECKS=0');
+      Schema::dropIfExists('departamentos');
+      DB::statement('SET FOREIGN_KEY_CHECKS=1');
+    }
+}
