5c435aa743109a82da64d75e6d2e615e51c381f0
Jhony Rocha <jhony.alexander.rocha@gmail.com>
2020-07-13 09:54:42 -0500

    Creando migración de la tabla municipios

---
+++ database/migrations/2020_06_21_120005_create_municipios_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+use App\Models\Municipio;
+
+class CreateMunicipiosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('municipios', function (Blueprint $table) {
+          $table->increments('id');
+          $table->unsignedInteger('departamento_id');
+          $table->string('nombre');
+          $table->string('codigo');
+          $table->string('divipola');
+          $table->boolean('activo')->default(Municipio::REGISTRO_ACTIVO);
+
+          $table->timestamps();
+          $table->softDeletes()->nullable();
+
+          $table->foreign('departamento_id')->references('id')->on('departamentos');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      DB::statement('SET FOREIGN_KEY_CHECKS=0');
+      Schema::dropIfExists('municipios');
+      DB::statement('SET FOREIGN_KEY_CHECKS=1');
+    }
+}
